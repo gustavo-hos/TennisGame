36fd8113e003860f143c333471288655300df65c
Gustavo Henrique <gustavo@terato.me>
2024-02-16 10:01:11 +0000

    delete: removed unnecessary Exception

--- src/main/java/me/terato/tennis_game/exception/InvalidGameInputException.java
+++
@@ -1,8 +0,0 @@
-package me.terato.tennis_game.exception;
-
-public class InvalidGameInputException extends Exception {
-
-    public InvalidGameInputException(String message) {
-        super(message);
-    }
-}

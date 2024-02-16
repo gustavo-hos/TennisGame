a476806ee0b3477d4d5fe856ebd18ce005b3ad35
Gustavo Henrique <gustavo@terato.me>
2024-02-16 10:05:16 +0000

    refactor: removed unused imports

--- src/main/java/me/terato/tennis_game/score/display/ScoreDisplay.java
+++ src/main/java/me/terato/tennis_game/score/display/ScoreDisplay.java
@@ -1,8 +1,5 @@
 package me.terato.tennis_game.score.display;
 
-import me.terato.tennis_game.score.PlayerScore;
-import me.terato.tennis_game.score.ScoreSystem;
-
 public interface ScoreDisplay {
 
     void displayScore();
